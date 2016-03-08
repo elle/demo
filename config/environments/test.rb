231a1d31b2b61ee1e895c750d721b898c71a70b2
Elle Meredith <ellemeredith@gmail.com>
2016-03-08 17:56:43 -0500

    configure active_job

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { host: "example.com" }
+  config.active_job.queue_adapter = :inline
 
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
